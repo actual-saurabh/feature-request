dbcbfeb7be7005aeb989db2c71d0550fdbfa9356
hosein <h.farahani@outlook.com>
2016-04-10 11:07:45 +0430

    Check data before using to avoid errors

--- admin/includes/class-avfr-settings.php
+++ admin/includes/class-avfr-settings.php
@@ -546,8 +546,11 @@ add_action('groups_add_form_fields', 'avfr_groups_edit_form_fields');
 
 function avfr_save_groups_custom_meta( $term_id ) {
 
-    $max_votes = abs($_POST['max-votes']);
-    $total_votes = abs($_POST['total-votes']);
+    $max_votes_val = isset( $_POST['max-votes'] ) ? : 0;
+    $total_votes_val = isset( $_POST['total-votes'] ) ? : 0;
+
+    $max_votes = abs($max_vote_val);
+    $total_votes = abs($total_votes_val);
     $comments_disabled = $_POST['cm-disabled'] ? 'on' : 'off' ;
     $new_disabled = $_POST['new-disabled'] ? 'on' : 'off' ;
 
